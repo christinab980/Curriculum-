32db6ab447cf7b49469050ffe380816d03b4d23d
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-01-24 18:25:11 -0800

    add JavaScipt Script2 ClassWork detailing objects and arrays

---
+++ Javascript/Data/Class-Js-Practice/script2.js
@@ -0,0 +1,131 @@
+const budget = [
+    {
+        name: 'keyboard',
+        cost: 120,
+        isDiscretionary: false
+    }, 
+    {
+        name: 'dog',
+        cost: 150,
+        isDiscretionary: false,
+    },
+    {
+        name: 'starbucks',
+        cost: 10,
+        isDiscretionary: true, 
+    },
+    {
+        name: 'New Car',
+        cost: 2550,
+        isDiscretionary: false,
+    },
+    {
+        name: 'dinner',
+        cost: 15,
+        isDiscretionary: true
+    }
+];
+
+
+
+const friends = [
+    {
+        first: 'Minnaret',
+        last: 'King',
+        over25: true,
+        hobbies: ['art', 'hiking', 'watching TV']
+    },
+    {
+        first:'Daryl',
+        last: 'King',
+        over25: true,
+        hobbies: ['reading', 'crafting', 'shopping']
+    },
+    {
+        first: 'Amelia',
+        last: 'Camp',
+        over25: true,
+        hobbies: ['TikTok', 'park dates', 'reality TV']
+    }
+];
+
+const rubiksCube = {
+    sides: 6,
+    colors: ['red', 'yellow', 'blue', 'green', 'white', 'orange'],
+    isHard: true,
+    fastestTime: '3.56 seconds',
+    howManyHaveBeenSold: '450 million',
+};
+
+const artists = [
+    {
+        name: 'Ed Sheeran',
+        genre: 'Pop',
+        grossSales: '150 million',
+        wonGrammy: true,
+        yearArtistWasBorn: 1991,
+        charities: [
+            {
+                redCross:false,
+                unifcef: false,
+                doctorsWithoutBorders: true,
+            }
+        ]
+    },
+    {
+        name: 'BTS',
+        genre: 'K-Pop',
+        grossSales: '300 million',
+        wonGrammy: false,
+        yearArtistWasBorn: 2013,
+        charities: [
+            {
+                redCross: false,
+                unifcef: true,
+                doctorsWithoutBorders: true,
+             }
+        ]
+    },
+    {
+        name: 'Taylor Swift',
+        genre: 'Pop',
+        grossSales: '250 million',
+        wonGrammy: true,
+        yearArtistWasBorn: 1989,
+        charities: [
+            {
+                redCross: false,
+                unifcef: true,
+                doctorsWithoutBorders: false,
+            }
+        ]
+    },
+    {
+        name: 'Paramore',
+        genre: 'Rock',
+        grossSales: '8 million',
+        wonGrammy: false,
+        yearArtistWasBorn: 2004,
+        charities: [
+            {
+                redCross: true,
+                unifcef: false,
+                doctorsWithoutBorders: false,
+            }
+        ]
+    },
+    {
+        name: 'Adele',
+        genre: 'Pop',
+        grossSales: '70 million',
+        wonGrammy: true,
+        yearArtistWasBorn: 1988,
+        charities: [
+            {
+                redCross: false,
+                unifcef: true,
+                doctorsWithoutBorders: true,
+            }
+        ]
+    }
+];
