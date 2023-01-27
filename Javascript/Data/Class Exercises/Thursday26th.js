89a92b5dbd961655a6cac4aacd01999288cc07c4
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-01-26 16:14:13 -0800

    added Class Exercises dealing with JS, HTML, and CSS

---
+++ Javascript/Data/Class Exercises/Thursday26th.js
@@ -0,0 +1,74 @@
+const months = [
+    {
+        monthNumber: 1,
+        monthAbbreviation: 'Jan',
+        monthName: "January",
+        monthDaysCount: 31,
+    }, 
+    {
+        monthNumber: 2,
+        monthAbbreviation: 'Feb',
+        monthName: 'February',
+        monthDaysCount: 28,
+    },
+    {
+        monthNumber: 3,
+        monthAbbreviation: 'Mar',
+        monthName:'March',
+        monthDaysCount: 31,
+    },
+    {
+        monthNumber: 4,
+        monthAbbreviation: 'Apr',
+        monthName: 'April',
+        monthDaysCount: 30,
+    },
+    {
+        monthNumber: 5,
+        monthAbbreviation: 'May',
+        monthName: 'May',
+        monthDaysCount: 31,
+    },
+    {
+        monthNumber: 6,
+        monthAbbreviation: 'Jun',
+        monthName: "June",
+        monthDaysCount: 30,
+    },
+    {
+        monthNumber: 7,
+        monthAbbreviation: 'Jul',
+        monthName: 'July',
+        monthDaysCount: 31,
+    }, 
+    {
+        monthNumber: 8, 
+        monthAbbreviation: 'Aug',
+        monthName: 'August',
+        monthDaysCount: 31,
+    },
+    {
+        monthNumber: 9, 
+        monthAbbreviation: 'Sept',
+        monthName: 'September',
+        monthDaysCount: 30,
+    },
+    {
+        monthNumber: 10, 
+        monthAbbreviation: 'Oct',
+        monthName: 'October',
+        monthDaysCount: 31,
+    },
+    {
+        monthNumber: 11, 
+        monthAbbreviation: 'Nov',
+        monthName: 'November',
+        monthDaysCount: 30
+    },
+    {
+        monthNumber: 12, 
+        monthAbbreviation: 'Dec',
+        monthName: 'December',
+        monthDaysCount: 31,
+    },
+];
