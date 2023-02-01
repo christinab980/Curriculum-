14fee4fea0b29c0cb4f73fc5081d930c8538a8e7
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-01-31 16:46:26 -0800

    adding Week 3 Homework file and JS Function Class Exercise

---
+++ Homework/Week3.md
@@ -0,0 +1,9 @@
+# Homework 
+## Week 3 
+
+| Assignment | Grade |
+|------------|:-----:| 
+|Sum two Numbers | 7.5 |
+|Custom Greeting | 
+|Area of Perimeter|
+|Student's Final Grade |
---
+++ Javascript/Data/Class Exercises/main.js
@@ -0,0 +1,19 @@
+function addr (n1,n2) {
+ return n1+n2
+}
+
+console.log(addr(2,7))
+
+function customGreeting (str1, str2) {
+    return 'str1 str2'
+}
+
+console.log(customGreeting('Hello', 'Christina'))
+
+function captialize (firstName) {
+    const firstLetterCaptialized = firstletter.toUpperCase();
+    const remainingcharacters = str.substring(1);
+    return firstLetterCaptialized + remainingcharacters;
+}
+
+firstName = "anna"
