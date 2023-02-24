df6fe5d5c3db47ab91ab6b1d687728ad3d6b55cf
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-02-23 17:18:09 -0800

    updated JS on Black Jack Project

--- Black Jack/main.js
+++ Black Jack/main.js
@@ -3,6 +3,7 @@ const playerHand = document.getElementById("player-hand");
 const deck = [];
 const suits = ["hearts", "spades", "clubs", "diamonds"];
 const ranks = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
+const deal = document.getElementById("deal-button")
 const makeDeck = (rank, suit) => {
   const card = {
     rank: rank,
@@ -21,3 +22,26 @@ for (let suit of suits) {
 window.addEventListener("DOMContentLoaded", () => {
   // Execute after page load
 });
+
+function deal () {
+    for (let i =1; i <=2; i++) {
+        const card = document.createElement("img");
+        card.src = "https://github.com/samuraijane/js-hw-blackjack-exercise/blob/main/images/10_of_clubs.png";
+        card.id = "card";
+        dealerHand.append(card);
+    }
+};
+
+function deal () {
+    for (let i =1; i <=2; i++) {
+        const card = document.createElement("img");
+        card.src = "https://github.com/samuraijane/js-hw-blackjack-exercise/blob/main/images/10_of_clubs.png";
+        card.id = "card";
+        playerHand.append(card);
+    }
+};
+
+deal.addEventListener('click', deal);
+
+
+
