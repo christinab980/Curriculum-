b8c788f09b0a8c69413b8579091161b1d7dba0cc
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-02-24 16:28:15 -0800

    updated JS in Black Jack Project

--- Black Jack/main.js
+++ Black Jack/main.js
@@ -2,66 +2,110 @@ const dealerHand = document.getElementById("dealer-hand");
 const playerHand = document.getElementById("player-hand");
 const dealButton = document.getElementById("deal-button");
 const hitButton = document.getElementById("hit-button");
-console.log(Deck);
+let isGameOver = false;
+let gameDeck = buildDeck();
+let pHand = [];
+let dHand = [];
 
-const createCard = (rank, suit) => {
-  const card = {
-    rank: rank,
-    suit: suit,
-    pointValue: rank > 10 ? 10 : rank,
-  };
-  return card;
-};
+
+const getCardImage = (card) => {
+    let 11 = 'jack'
+    const cardImage = document.createElement("img");
+    if (card.rank === 1) {
+        cardImage.setAttribute('src',`images/ace_of_${card.suit}.png`)
+    }
+    if (card.rank === 11) {
+        cardImage.setAttribute('src',`images/jack_of_${card.suit}.png`)
+    }
+    if (card.rank === 13) {
+        cardImage.setAttribute('src',`images/queen_of_${card.suit}.png`)
+    }
+    if (card.rank === 14) {
+        cardImage.setAttribute('src',`images/king_of_${card.suit}.png`)
+    } 
+    else  {
+        cardImage.setAttribute('src',`images/${card.rank}_of_${card.suit}.png`)
+    }
+    return cardImage
+}; 
 
 function buildDeck () {
     const deck = [];  
     const suits = ["hearts", "spades", "clubs", "diamonds"];
     const ranks = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
 
-    for (let suit of suits) {
-        for (const rank of ranks) {
-            const card = createCard(rank,suit);
-            deck.push(card);
+    for (let i=0; i<suits.length; i++) {
+        const suit = suits[i]
+        for (let rank = 1; rank<=14; rank++) {
+            const card = {
+                rank: rank,
+                suit: suit,
+            }
+            deck.push(card)
         }
     }    
     return deck;
 };
 
-window.addEventListener("DOMContentLoaded", () => {
-  // Execute after page load
-});
-
-function hand () {
-    for (let i =1; i <=2; i++) {
-        const card = document.createElement("img");
-        const card2 = document.createElement("img");
-        //card.src = "https://raw.githubusercontent.com/samuraijane/js-hw-blackjack-exercise/main/images/10_of_clubs.png"
-        //card2.src = 
-        //card.id = "card";
-        //dealerHand.append(card);
+function displayGame () {
+    dealerHand.innerHTML = " "
+    playerHand.innerHTML = " "
+    for (let i=0; i<dHand.length; i++) {
+        const newCard = dHand[i]
+        const cardElement = getCardImage(newCard)
+        dealerHand.appendChild(cardElement)
+    }
+    for (let i=0; i<pHand.length; i++) {
+        const newCard = pHand[i]
+        const cardElement = getCardImage(newCard)
+        playerHand.appendChild(cardElement)
     }
 };
 
-function playersHand () {
-    for (let i =1; i <=2; i++) {
-        const card = document.createElement("img");
-        //card.src = "https://raw.githubusercontent.com/samuraijane/js-hw-blackjack-exercise/main/images/10_of_clubs.png"
-        //card.id = "card";
-        playerHand.append(card);
-    }
+function deal (deck, target) {
+    const currentCard = deck.pop()
+    target.push(currentCard)
 };
 
-function hitMe () {
-    for (let i=1; i<=1; i++) {
-        const card = document.createElement("img");
-        card.src = "https://raw.githubusercontent.com/samuraijane/js-hw-blackjack-exercise/main/images/10_of_clubs.png"
-        card.id = "card";
-        playerHand.append(card);
-    }
-};
+// window.addEventListener("DOMContentLoaded", () => {
+//   // Execute after page load
+// });
 
-dealButton.addEventListener('click', hand);
-dealButton.addEventListener('click', playersHand);
-hitButton.addEventListener('click', hitMe);
+// function hand () {
+//     for (let i =1; i <=2; i++) {
+//         const card = document.createElement("img");
+//         card.src = "https://raw.githubusercontent.com/samuraijane/js-hw-blackjack-exercise/main/images/10_of_clubs.png"
+//         card.id = "card";
+//         dealerHand.append(card);
+//     }
+// };
 
+// function playersHand () {
+//     for (let i =1; i <=2; i++) {
+//         const card = document.createElement("img");
+//         card.src = "https://raw.githubusercontent.com/samuraijane/js-hw-blackjack-exercise/main/images/10_of_clubs.png"
+//         card.id = "card";
+//         playerHand.append(card);
+//     }
+// };
 
+// function hitMe () {
+//     for (let i=1; i<=1; i++) {
+//         const card = document.createElement("img");
+//         card.src = "https://raw.githubusercontent.com/samuraijane/js-hw-blackjack-exercise/main/images/10_of_clubs.png"
+//         card.id = "card";
+//         playerHand.append(card);
+//     }
+// };
+
+// dealButton.addEventListener('click', hand);
+// dealButton.addEventListener('click', playersHand);
+// hitButton.addEventListener('click', hitMe);
+
+dealButton.addEventListener('click', function(){
+    deal(gameDeck, pHand) 
+    deal(gameDeck, dHand)
+    deal(gameDeck, pHand) 
+    deal(gameDeck, dHand)  
+    displayGame()
+})
