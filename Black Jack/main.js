3cb445de228c5d5f2ee54b2c8d155f9ec9adc47f
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-02-24 16:40:30 -0800

    updated JS on Black Jack Project

--- Black Jack/main.js
+++ Black Jack/main.js
@@ -2,6 +2,7 @@ const dealerHand = document.getElementById("dealer-hand");
 const playerHand = document.getElementById("player-hand");
 const dealButton = document.getElementById("deal-button");
 const hitButton = document.getElementById("hit-button");
+const rank = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
 let isGameOver = false;
 let gameDeck = buildDeck();
 let pHand = [];
@@ -9,7 +10,6 @@ let dHand = [];
 
 
 const getCardImage = (card) => {
-    let 11 = 'jack'
     const cardImage = document.createElement("img");
     if (card.rank === 1) {
         cardImage.setAttribute('src',`images/ace_of_${card.suit}.png`)
@@ -17,13 +17,13 @@ const getCardImage = (card) => {
     if (card.rank === 11) {
         cardImage.setAttribute('src',`images/jack_of_${card.suit}.png`)
     }
-    if (card.rank === 13) {
+    if (card.rank === 12) {
         cardImage.setAttribute('src',`images/queen_of_${card.suit}.png`)
     }
-    if (card.rank === 14) {
+    if (card.rank === 13) {
         cardImage.setAttribute('src',`images/king_of_${card.suit}.png`)
     } 
-    else  {
+    if (card.rank <11 && card.rank >1)  {
         cardImage.setAttribute('src',`images/${card.rank}_of_${card.suit}.png`)
     }
     return cardImage
@@ -32,11 +32,10 @@ const getCardImage = (card) => {
 function buildDeck () {
     const deck = [];  
     const suits = ["hearts", "spades", "clubs", "diamonds"];
-    const ranks = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
 
     for (let i=0; i<suits.length; i++) {
         const suit = suits[i]
-        for (let rank = 1; rank<=14; rank++) {
+        for (let rank = 1; rank<=13; rank++) {
             const card = {
                 rank: rank,
                 suit: suit,
