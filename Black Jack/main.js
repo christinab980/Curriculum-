d3f0f9b9f5510f91cbb432baa51271f689fa98a4
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-02-27 20:26:52 -0800

    added image and updated both JS and HTML for BlackJack Project

--- Black Jack/main.js
+++ Black Jack/main.js
@@ -1,6 +1,7 @@
 const dealerHand = document.getElementById("dealer-hand");
 const playerHand = document.getElementById("player-hand");
 const dealButton = document.getElementById("deal-button");
+const standButton = document.getElementById ("stand-button")
 const hitButton = document.getElementById("hit-button");
 const rank = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
 let isGameOver = false;
@@ -8,6 +9,21 @@ let gameDeck = buildDeck();
 let pHand = [];
 let dHand = [];
 
+// let dealersSum = 0;
+// let yourSum = 0;
+
+// let dealerAceCount = 0;
+// let yourAceCount = 0;
+
+// let hidden;
+// let deck;
+// let canHit = true;
+
+// window.onload = () => {
+//     buildDeck();
+//     shuffleDeck();
+//     startGame();
+// }
 
 const getCardImage = (card) => {
     const cardImage = document.createElement("img");
@@ -47,7 +63,7 @@ function buildDeck () {
     return deck;
 };
 
-function displayGame () {
+const displayGame = () => {
     dealerHand.innerHTML = " "
     playerHand.innerHTML = " "
     for (let i=0; i<dHand.length; i++) {
@@ -61,6 +77,18 @@ function displayGame () {
         playerHand.appendChild(cardElement)
     }
 };
+
+// function firstDeal () {
+//     let recipient = 'player'
+//     for (let i =0; i < 4; i++ ) {
+//     displayGame(deck.pop(), recipient);
+//     if (recipient === 'player') {
+//         recipient = 'dealer';
+//     } else {
+//         recipient = 'player'
+//     }
+// }
+// }
 
 function deal (deck, target) {
     const currentCard = deck.pop()
@@ -76,11 +104,86 @@ function shuffleDeck (deck) {
     }
 }
 
+// function startGame() {
+//     hidden = deck.pop();
+//     dealersSum += getValue(hidden);
+//     dealerAceCount += checkAce(hidden);
+
+//     while (dealerSum < 17) {
+//         let cardImage = document.createElement("img");
+//         let card = deck.pop;
+//         cardImage.scr ="./images/" + card + ".png";
+//         dealersSum += getValue(card);
+//         dealerAceCount += checkAce(card);
+//         document.getElementById('dealer-hand').append(cardImage)
+//     }
+
+//     for (let i=0; i < 2; i++) {
+//         let cardImage = document.createElement("img");
+//         let card = deck.pop;
+//         cardImage.scr ="./images/" + card + ".png";
+//         yourSum += getValue(card);
+//         yourAceCount += checkAce(card);
+//         document.getElementById('player-hand').append(cardImage)
+//     }
+// }
+
 function hitMe () {
   for (let i=0; i<1; i++) {
-    renderCard(deck.pop(card), pla)
+    const newCard = gameDeck[i]
+    const cardElement = getCardImage(newCard)
+    playerHand.appendChild(cardElement)
   }
 }
+
+// function standByMe () {
+//     dealerSum = reduceAceCount(dealerSum, dealerAceCount);
+//     yourSum = reduceAce(yourSum, yourAceCount);
+
+//     hitMe = false;
+    
+//     let message = " " 
+//     if (yourSum > 21) {
+//         message = "You Lose!";
+//     } else if (dealerSum > 21) {
+//         message = "You Win";
+//     } else if (yourSum == dealerSum) {
+//         message = "Tie";
+//     } else if (yourSum < dealerSum) {
+//         message = "You Lose!";
+//     }
+//     document.getElementById('dealer-points').innerText = dealerSum;
+//     document.getElementById('player-points').innerText = yourSum
+//     document.getElementById('result').innerText = message;
+// };
+
+// function getValue (card) {
+//     let data = card.split('_');
+//     let value = data[0];
+
+//     if (isNaN(value)) {
+//         if (value == 'ace') {
+//             return 11;
+//         } 
+//         return 10;
+//     }
+//     return parseInt(value);
+// }
+
+// function checkAce (card) {
+//     if (card[0] == "Ace") {
+//         return = 1;
+//     } 
+//     return 0;
+// }
+
+// function reduceAce(playerSum, playerAceCount) {
+//     while(playerSum > 21 && playerAceCount > 0) {
+//         playerSum -= 10;
+//         playerAceCount -= 1;
+//     }
+//     return playerSum
+// }
 
 shuffleDeck(gameDeck);
 
@@ -93,3 +196,4 @@ dealButton.addEventListener('click', function(){
 })
 
 hitButton.addEventListener('click', hitMe)
+standButton.addEventListener('click', standByMe)
