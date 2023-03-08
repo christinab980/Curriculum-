5535ff18b95f60fde616c10e7d1674cf6bf7cafe
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-03-07 18:28:26 -0800

    updated Dog CEO 4 JS, HTML and CSS

--- Assignments/Dog CEO/main.js
+++ Assignments/Dog CEO/main.js
@@ -1,55 +1,60 @@
 const button = document.getElementById('dogButton');
 const output = document.getElementById('output');
-const dogQuestion = document.getElementById('question')
+const thePrompt = document.getElementById('question');
 const fetchUrl = "https://dog.ceo/api/breeds/image/random"
+
+let breeds = [];
+let answer = '';
+
+output.addEventListener('click', handleSelection)
 
 //promise to get the dog API 
 const customPromise = async() => {
   const response = await fetch (fetchUrl);
   const data = await response.json();
   const url = await data.message;
-  console.log(data.message)
-  return url;
+  const breed = url.split("/")[4];
+  return {url, breed};
 };
 
 //creating the image from the DOM to turn the API into an actual image
 const dogImg = async() => {
-    const list = [];
+    output.innerHTML = '';
     for(i = 0; i<4; i++) {
-      const dogImgUrl = customPromise();
+      const div = document.createElement('div');
+      div.className = 'dog-img'
       const dog = document.createElement('img');
-      const url = await customPromise();
+      const {url, breed} = await customPromise();
+      breeds.push(breed);
       dog.src = url;
-      output.append(dog);
-      list.push(dogImgUrl);
+      dog.setAttribute('data-breed', breed)
+      div.append(dog);
+      output.append(div); 
   }
-  return list
+  setRandomAnswer();
 }
 
-//creating a question about the breeds to the viewer and then stating if it is right/wrong 
-const breedsQuestion = async() => {
-    const urlList = dogImg();
-    const prompt = document.createElement('div');
-    for (let i=0; i<=4, i++;) {
-      const dogImgBreeds = await urlList
-      const findbreed = dogImgBreeds[Math.floor(Math.random() * dogImgBreeds.length)];
-      const breedindex = findbreed.split('/');
-      const whichBreed = breedindex[2];
-      prompt.innerText(`Which dog is ${whichBreed}`)
-    }
-    return prompt
+function handleSelection(e) {
+  const breed = e.target.getAttribute('data-breed');
+  if (breed) {
+    giveFeedback(breed === answer)
+
+  }
+
 }
 
-const breedsAnswer = e => {
-  let breed = breedsQuestion
-  const answer = document.createElement('div');
-  if (e.target.matches === `${breed}`) {
-    answer.innerText('Correct!');
-    header.append(answer);
-  } else {
-    answer.innerText('Wrong');
-    header.append(answer);
-  }
+function setRandomAnswer() {
+  answer = breeds[Math.floor(Math.random()* breeds.length)];
+  const prompt = document.createElement('div');
+  prompt.innerText = `Which image shows the ${answer} breed?`
+  thePrompt.append(prompt);
+}
+
+function giveFeedback(isCorrect) {
+  const feedback = document.createElement('div');
+  const message = isCorrect ? 'CORRECT' : 'INCORRECT'
+  feedback.innerText = `That is ${message}`;
+  thePrompt.append(feedback)
 }
 
 button.addEventListener('click', dogImg);
