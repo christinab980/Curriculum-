b0e963195d15b64184d5b5a78e2b200f9e13d9db
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-04-14 13:37:33 -0700

    Added Server 101 notes and demo

---
+++ server/101/demo/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+  res.json({message: "Hello Worlds."});
+});
+
+server.listen (8080, () => {
+  console.log('The server is running at PORT 8080')
+})
