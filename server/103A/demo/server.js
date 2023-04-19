df3e7b9b58602f3fbf78c8518f590ce4d3e98fe9
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-04-18 18:27:11 -0700

    updated files

--- server/103A/demo/server.js
+++ server/103A/demo/server.js
@@ -6,26 +6,26 @@ const server = express();
 server.use(express.json());
 server.use(cookieParser());
 
-server.get('/', (req, rest) => {
+server.get('/', (req, res) => {
   res.json({message: 'You are at the root'})
 });
 
-server.post('/body', (req, rest) => {
+server.post('/body', (req, res) => {
   console.log('body', req.body);
   res.json({message: 'You are at the /body'})
 });
 
-server.get('/cookies', (req, rest) => {
+server.get('/cookies', (req, res) => {
   console.log('cookies', req.cookies);
   res.json({message: 'You are at the /cookies'})
 });
 
-server.get('/params/:someRoute', (req, rest) => {
+server.get('/params/:someRoute', (req, res) => {
   console.log('params', req.params);
   res.json({message: 'You are at the /params'})
 });
 
-server.get('/query', (req, rest) => {
+server.get('/query', (req, res) => {
   console.log('query', req.query);
   res.json({message: 'You are at the /query'})
 });
